bc4d3bdf44fd602dce56fa451d1279eb85dcd17a
Craig McCown <craigrmccown@gmail.com>
2019-03-27 16:59:08 -0400

    Create tunnel to external proxy on api startup

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,12 +1,28 @@
 import express from 'express'
+import ngrok from 'ngrok'
 
-const app = express()
 const { PORT } = process.env
 
 if (PORT == null) {
   throw new Error('Environment variable PORT must be set!')
 }
 
-app.get('/', (req, res) => res.send('Hello World!'))
+/**
+ * Ideally, we should only open a tunnel to an external proxy service during local development. To
+ * fix this, we should abstract the function that sets up the app and only call `ngrok.connect`
+ * when NODE_ENV !== 'production'. Furthermore, the proxy should run as a separate process so that
+ * it does not restart every time a file is changed.
+ */
+ngrok.connect(PORT).then(url => {
+  const app = express()
 
-app.listen(PORT, () => console.info(`Listening on port ${PORT}!`)) // eslint-disable-line no-console
+  app.set('PORT', PORT)
+  app.set('WEBHOOK_URL', url)
+
+  app.get('/', (req, res) => res.send('Hello World!'))
+
+  app.listen(app.get('PORT'), () =>
+    // eslint-disable-next-line no-console
+    console.info(`Listening on port ${app.get('PORT')}, available at ${app.get('WEBHOOK_URL')}!`),
+  )
+})
