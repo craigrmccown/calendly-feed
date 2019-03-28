100de27ce5c3c2e75b33ee787fc6a5c3050bcbb1
Craig McCown <craigrmccown@gmail.com>
2019-03-28 06:25:37 -0400

    Clean up webhooks on startup and shutdown

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -13,6 +13,8 @@ if (PORT == null || CALENDLY_API_TOKEN == null) {
   throw new Error('Environment variables PORT and CALENDLY_API_TOKEN must be set!')
 }
 
+const calendly = new Calendly(CALENDLY_API_TOKEN)
+
 /**
  * Ideally, we should only open a tunnel to an external proxy service during local development. To
  * fix this, we should abstract the function that sets up the app and only call `ngrok.connect`
@@ -20,12 +22,15 @@ if (PORT == null || CALENDLY_API_TOKEN == null) {
  * it does not restart every time a file is changed.
  */
 ngrok.connect(parseInt(PORT, 10)).then(async url => {
+  calendly.initialize({ context: {} }, undefined)
+
+  await calendly.deleteWebhookSubscriptions()
+  await calendly.createWebhookSubscription(`${url}/invites`)
+
   const typeDefs = gql`
     ${await concatFiles(path.join(__dirname, './schema'))}
   `
-  const dataSources = () => ({ calendly: new Calendly(CALENDLY_API_TOKEN) })
-
-  const apollo = new ApolloServer({ typeDefs, resolvers, dataSources })
+  const apollo = new ApolloServer({ typeDefs, resolvers, dataSources: () => ({ calendly }) })
   const app = express()
 
   app.set('PORT', PORT)
@@ -39,4 +44,13 @@ ngrok.connect(parseInt(PORT, 10)).then(async url => {
     // eslint-disable-next-line no-console
     console.info(`Listening on port ${app.get('PORT')}, available at ${app.get('WEBHOOK_URL')}!`),
   )
+})
+
+process.on('SIGTERM', async () => {
+  // eslint-disable-next-line no-console
+  console.info('Shutting down gracefully...')
+
+  await calendly.deleteWebhookSubscriptions()
+
+  process.exit(0)
 })
