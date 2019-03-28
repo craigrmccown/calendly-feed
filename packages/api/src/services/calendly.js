3de272e98d1b763021a44fb9a3365cd81204e260
Craig McCown <craigrmccown@gmail.com>
2019-03-28 05:44:28 -0400

    Implement Calendly webhook subscription API

--- packages/api/src/services/calendly.js
+++ packages/api/src/services/calendly.js
@@ -1,5 +1,13 @@
 import { RESTDataSource } from 'apollo-datasource-rest'
 
+const createFormData = pairs => {
+  const fd = new URLSearchParams()
+
+  pairs.forEach(([k, v]) => fd.append(k, v))
+
+  return fd
+}
+
 export default class Calendly extends RESTDataSource {
   constructor(token) {
     super()
@@ -11,7 +19,32 @@ export default class Calendly extends RESTDataSource {
     request.headers.set('X-TOKEN', this.token)
   }
 
-  async getUser() {
+  getUser() {
     return this.get('users/me')
+  }
+
+  getWebhookSubscriptions() {
+    return this.get('hooks')
+  }
+
+  createWebhookSubscription(url) {
+    return this.post(
+      'hooks',
+      createFormData([
+        ['url', url],
+        ['events[]', 'invitee.created'],
+        ['events[]', 'invitee.canceled'],
+      ]),
+    )
+  }
+
+  deleteWebhookSubscription(id) {
+    return this.delete(`hooks/${id}`)
+  }
+
+  async deleteWebhookSubscriptions() {
+    const { data: subscriptions } = await this.getWebhookSubscriptions()
+
+    return Promise.all(subscriptions.map(({ id }) => this.deleteWebhookSubscription(id)))
   }
 }
--- packages/api/src/services/calendly.test.js
+++ packages/api/src/services/calendly.test.js
@@ -3,9 +3,14 @@ import Calendly from './calendly'
 describe('Calendly', () => {
   const token = 'abcd1234'
   const calendly = new Calendly(token)
+  const getResult = Promise.resolve({})
+  const postResult = Promise.resolve({})
+  const deleteResult = Promise.resolve({})
 
   beforeEach(() => {
-    calendly.get = jest.fn()
+    calendly.get = jest.fn().mockReturnValue(getResult)
+    calendly.post = jest.fn().mockReturnValue(postResult)
+    calendly.delete = jest.fn().mockReturnValue(deleteResult)
   })
 
   test('it sets the correct base url', () => {
@@ -20,9 +25,79 @@ describe('Calendly', () => {
   })
 
   describe('getUser', () => {
-    test('it uses the correct path', () => {
+    test('it passes the correct arguments', () => {
       calendly.getUser()
       expect(calendly.get).toHaveBeenCalledWith('users/me')
+    })
+
+    test('it returns the results of the request', () => {
+      const results = calendly.getUser()
+
+      expect(results).toBe(getResult)
+    })
+  })
+
+  describe('getWebhookSubscriptions', () => {
+    test('it passes the correct arguments', () => {
+      calendly.getWebhookSubscriptions()
+      expect(calendly.get).toHaveBeenCalledWith('hooks')
+    })
+
+    test('it returns the results of the request', () => {
+      const results = calendly.getWebhookSubscriptions()
+
+      expect(results).toBe(getResult)
+    })
+  })
+
+  describe('createWebhookSubscription', () => {
+    test('it passes the correct arguments', () => {
+      calendly.createWebhookSubscription('http://foo.bar')
+
+      const [[path, body]] = calendly.post.mock.calls
+
+      expect(path).toBe('hooks')
+      expect(body.getAll('url')).toEqual(['http://foo.bar'])
+      expect(body.getAll('events[]').sort()).toEqual(['invitee.created', 'invitee.canceled'].sort())
+    })
+
+    test('it returns the results of the request', () => {
+      const results = calendly.createWebhookSubscription('http://foo.bar')
+
+      expect(results).toBe(postResult)
+    })
+  })
+
+  describe('deleteWebhookSubscription', () => {
+    test('it passes the correct arguments', () => {
+      calendly.deleteWebhookSubscription('123')
+      expect(calendly.delete).toHaveBeenCalledWith('hooks/123')
+    })
+
+    test('it returns the results of the request', () => {
+      const results = calendly.deleteWebhookSubscription('123')
+
+      expect(results).toBe(deleteResult)
+    })
+  })
+
+  describe('deleteWebhookSubscriptions', () => {
+    beforeEach(() => {
+      calendly.get.mockReturnValue(Promise.resolve({ data: [{ id: '123' }, { id: '456' }] }))
+    })
+
+    test('it gets all webhook subscriptions and deletes each one', async () => {
+      await calendly.deleteWebhookSubscriptions()
+
+      expect(calendly.delete).toHaveBeenCalledWith('hooks/123')
+      expect(calendly.delete).toHaveBeenCalledWith('hooks/456')
+    })
+
+    test('it returns a list of delete responses', async () => {
+      const result = await calendly.deleteWebhookSubscriptions()
+      const innerResult = await deleteResult
+
+      expect(result).toEqual([innerResult, innerResult])
     })
   })
 })
